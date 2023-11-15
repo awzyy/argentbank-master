06d004bb8949b727fa5f049aa6b086b15361c10b
awzyy <waysbdo@gmail.com>
2023-11-15 11:19:47 +0100

    account placeholders

--- Front/src/pages/Dashboard.jsx
+++ Front/src/pages/Dashboard.jsx
@@ -92,16 +92,16 @@ function Dashboard() {
         ) : null}
 
         <Account
-          title="Argent Bank Checking (x8349)"
-          amount="$2,082.79"
+          title="Argent Bank Checking (x3448)"
+          amount="$48,098.43"
         />
         <Account
-          title="Argent Bank Savings (x6712)"
-          amount="$10,928.42"
+          title="Argent Bank Checking (x3448)"
+          amount="$48,098.43"
         />
         <Account
-          title="Argent Bank Credit Card (x8349)"
-          amount="$184.30"
+          title="Argent Bank Checking (x3448)"
+          amount="$48,098.43"
         />
       </main>
       <Footer />
